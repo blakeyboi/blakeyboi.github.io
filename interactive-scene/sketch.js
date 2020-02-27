09aafad1a6f634ae55652ae5cf6612875f9a90ca
blakeyboi <45469497+blakeyboi@users.noreply.github.com>
2020-02-27 15:21:47 -0600

    fixed jumping and made wall kind of

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -11,6 +11,12 @@ let marioX;
 let marioY;
 let marioDY = 5;
 let movingUp = false;
+let movingDown = false;
+let wallX;
+let wallY;
+let wallW = 50;
+let wallH = 50;
+let wallDX = 5;
 
 function preload() {
   mario = loadImage("assets/mario.png");
@@ -20,35 +26,65 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  marioX = width;
-  marioY = height;
+  marioX = width/7;
+  marioY = height - height/7;
+  wallX = width;
 }
 // draw function
 function draw() {
   background(220);
   displayMario();
   moveMario();
+  displayWall();
+  moveWall();
 }
 // moving with keys
 function keyPressed() {
   if (key === " ") {
     movingUp = true;
+    movingDown = false;
   }
 }
 
 function keyReleased() {
   if (key === " ") {
+    movingDown = true;
     movingUp = false;
   }
 }
 
 function moveMario() {
-  if (movingUp) {
-    marioY -= marioDY;
+  if (marioY <= height - height/7) {
+    if (movingUp) {
+      marioY -= marioDY;
+    }
+    if (movingDown) {
+      marioY += marioDY;
+    }
+  }
+  else {
+    movingUp = false;
+    movingDown = false;
+    marioY = height - height/7;
   }
 }
-
+// making mario appear
 function displayMario() {
   image(mario, marioX, marioY, 100, 100);
+}
+
+// making walls move
+function moveWall() {
+  wallY = random(height/5, height);
+  // if (wallX >= 0) {
+    wallX -= wallDX;
+  // }
+}
+
+// making walls appear
+function displayWall() {
+  fill("black");
+  rect(wallX, wallY, wallW, wallH);
+
 }
 
