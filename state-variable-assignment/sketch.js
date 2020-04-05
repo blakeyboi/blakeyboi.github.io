4dddb171b9e486a8d7e5071b69a14fbfd6c0e9ac
blakeyboi <45469497+blakeyboi@users.noreply.github.com>
2020-04-05 16:06:43 -0600

    idk what this is

---
+++ state-variable-assignment/sketch.js
@@ -0,0 +1,17 @@
+// State Variable Assignment
+// Blake Tierney
+// April 8, 2020
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+}
+
