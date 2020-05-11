2a2fd3e04cfe57a414b6fd4798d7511b9b630328
blakeyboi <45469497+blakeyboi@users.noreply.github.com>
2020-05-10 20:45:58 -0600

    fully functional with sound

--- connect4-game/sketch.js
+++ connect4-game/sketch.js
@@ -6,27 +6,43 @@
 // - describe what you did to take this project "above and beyond"
 
 let screen = 1;
-let w = 7;
-let h = 6;
+let w;
+let h;
 let bs = 100;
 let dis = 10;
 let mob;
 let morb;
 let player;
 let board;
+let colorPicker;
+let colorPicker1;
+let colorPicker2;
+let sound;
 
-function make2DArray(h,w){
-  let arr = new Array(w);
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(h);
-  }
-  return arr;
+
+
+function preload() {
+  soundFormats('mp3', 'ogg');
+  sound = loadSound('assets/slot.mp3');
+  sound1 = loadSound('assets/slots.mp3');
+  sound2 = loadSound('assets/win');
+  sound.setVolume(0.1);
+  sound1.setVolume(0.1);
+  sound2.setVolume(0.1);
 }
 
 function setup() {
+  w = 7
+  h = 6
   createCanvas(w*bs, h*bs);
   ellipseMode(CORNER);
   textAlign(CENTER);
+  // colorPicker = createColorPicker('#ed225d');
+  // colorPicker.position(width/2+dis*10, height/2 + dis*2);
+  // colorPicker1 = createColorPicker('#ed225d');
+  // colorPicker1.position(width/2-dis*6, height/2 + dis/3);
+  // colorPicker2 = createColorPicker('#ed225d');
+  // colorPicker2.position(width/2+dis*11, height/2 + dis/3);
   player = 1;
   board = make2DArray(h, w);
   for (let y = 0; y < h; y++) {
@@ -34,6 +50,14 @@ function setup() {
       board[y][x] = 0;
     }
   }
+}
+
+function make2DArray(h,w){
+  let arr = new Array(w);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(h);
+  }
+  return arr;
 }
 
 function startScreen() {
@@ -45,8 +69,9 @@ function startScreen() {
   fill(120);
   textSize(dis*2);
   text("Instructions", width/2, height/2 - dis*2);
-  text("Click to place a chip", width/2, height/2)
-  text("Player 1 = RED, Player 2 = Blue", width/2, height/2 + dis*2)
+  text("Click to place a chip", width/2, height/2);
+  text("Player 1 = RED      Player 2 = Blue", width/2, height/2 + dis*2);
+  //text("Pick the board color = ", width/2, height/2 +dis*4);
   mob = collidePointRect(mouseX, mouseY, width/2 - width/10, height*3/4 - height/12, width/5, height/6);
   if (!mob) {
     fill(120);
@@ -79,6 +104,7 @@ function endScreen() {
   else {
     fill(255);
   }
+  stroke(0);
   rect(width/2, height*3/4, width/5, height/6);
   fill(0);
   textSize(dis*2);
@@ -110,10 +136,12 @@ function mousePressed() {
       if (player === 1) {
         board[y][x] = 1;
         player = 2;
+        sound.play();
       }
       else {
         board[y][x] = 2;
         player = 1;
+        sound1.play();
       }
     }
   }
@@ -131,23 +159,28 @@ function mousePressed() {
 function dropCoin() {
   rectMode(CORNER);
   noStroke();
+  //background(colorPicker.color());
   background("yellow");
   for (let y = 0; y < h; y++) {
     for (let x = 0; x < w; x++) {
       let i = x * bs;
       let j = y * bs;
-      stroke(1);
-      fill(255);
-      ellipse(i, j, bs, bs)
+      stroke(255, 204, 0);
+      fill(200);
+      ellipse(i + dis, j + dis, bs - dis*2, bs - dis*2)
       if (board[y][x] > 0) {
         if (board[y][x] === 1) {
+          //fill(colorPicker1.color());
+          stroke(255);
           fill("red");
         }
         if (board[y][x] === 2) {
+          //ill(colorPicker2.color());
+          stroke(255);
           fill("blue");
         }
-        ellipse(x*bs, y*bs, bs, bs);
-        ellipse(x*bs + dis, y*bs + dis, bs - dis*2, bs - dis*2);
+        ellipse(x*bs +dis, y*bs +dis, bs - dis*2, bs - dis*2);
+        ellipse(x*bs + dis*2, y*bs + dis*2, bs - dis*4, bs - dis*4);
       }
     }
   }
@@ -162,6 +195,7 @@ function draw() {
       dropCoin();
     }
     else {
+      sound2.play();
       screen = 3;
     }
   }
@@ -188,19 +222,19 @@ function getWinner() {
   }
   for(let y = 0; y < h; y++) {
     for(let x = 0; x < w; x++) {
-      // chceking vertically for 4 in a row
-      if(p(y,x) !== 0 && p(y,x) === p(y+1,x) && p(y,x) === p(y+2,x) && p(y,x) === p(y+3,x)) {
-        return p(y, x);
+      // cheking diagonally for 4 in a row, use d as direction
+      for(let d = -1; d <= 1; d += 2) {
+        if(p(y,x) !== 0 && p(y,x) === p(y+1*d,x+1) && p(y,x) === p(y+2*d,x+2) && p(y,x) === p(y+3*d,x+3)) {
+          return p(y, x);
+        }
       }
     }
   }
   for(let y = 0; y < h; y++) {
     for(let x = 0; x < w; x++) {
-      // cheking diagonally for 4 in a row, use d as direction
-      for(let d = -1; d < 1; d += 2) {
-        if(p(y,x) !== 0 && p(y,x) === p(y+1*d,x+1) && p(y,x) === p(y+2*d,x+2) && p(y,x) === p(y+3*d,x+3)) {
-          return p(y, x);
-        }
+      // chceking vertically for 4 in a row
+      if(p(y,x) !== 0 && p(y,x) === p(y+1,x) && p(y,x) === p(y+2,x) && p(y,x) === p(y+3,x)) {
+        return p(y, x);
       }
     }
   }
